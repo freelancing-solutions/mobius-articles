bb2770e235c017f1d300705ff37371b876a78e22
justice <mobiusndou@gmail.com>
2019-10-06 22:25:39 +0200

    Update web.js

--- web.js
+++ web.js
@@ -31,5 +31,5 @@ const results = news.search("bitcoin");
 
 app.listen(PORT).on('listening', () => {
     console.log(`Realtime server running on ${PORT} `);
-    console.log(results.payload);
+    console.log(results.payload[2]);
 });
