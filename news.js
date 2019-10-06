a45281a95e727605cfc750f51a8e7e5887d7f3b4
justice <mobiusndou@gmail.com>
2019-10-06 22:44:36 +0200

    Update news.js

--- news.js
+++ news.js
@@ -5,26 +5,40 @@ const newsapi = new NewsAPI(
 );
 
 const news_articles = [];
+const fetch_date = null;
 
 const searchNews = async term => {
     const results = {status:false,payload:[],error:{}};        
         const today = moment().format("YYYY-MM-DD");
-        await newsapi.v2.everything({
-            q: term,        
-            from: today,    
-            language: "en",
-            sortBy: "relevancy",    
-        }).then(response_json => {
-            console.log(response_json);
-            const articles = JSON.parse(response_json);        
-            results.payload =[...articles.articles];
+
+        if ((fetch_date === null) || (fetch_date !== today)){
+            await newsapi.v2.everything({
+                q: term,        
+                from: today,    
+                language: "en",
+                sortBy: "relevancy",    
+            
+            }).then(response_json => {
+                
+                console.log(response_json);
+                const articles = JSON.parse(response_json);        
+                results.payload =[...articles.articles];
+                results.status = true;
+                news_articles = results.payload;
+                fetch_date = today;
+            }).catch(error => {
+                console.log(error);
+                results.payload = [];
+                results.error = {...error};
+                results.status = false;
+            });
+
+        }else{
+            results.payload = news_articles;
             results.status = true;
-        }).catch(error => {
-            console.log(error);
-            results.payload = [];
-            results.error = {...error};
-            results.status = false;
-        });
+            results.error = {};
+        }
+
     return results;
 };
 
