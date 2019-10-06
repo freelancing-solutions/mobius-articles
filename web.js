9f0b670704003cab34a9ecd3e6bb37f0307bd0ef
justice <mobiusndou@gmail.com>
2019-10-07 00:49:36 +0200

    news api requests changed to crud api

--- web.js
+++ web.js
@@ -1,49 +1,30 @@
 
-const feathers = require('@feathersjs/feathers');
-const express = require('@feathersjs/express');
-const socketio = require('@feathersjs/socketio');
+
+const express = require('express');
+const bodyParser = require("body-parser");
 const news = require('./news');
 const PORT = process.env.PORT || 3030;
 
 
-// Search Services
+// create express app
+const app = express();
 
-class SearchService {
-  
-  constructor(){
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
 
-  }
-
-  async refine(data){
-    return news.refine(data);
-  }
-
-};
-
-
-// initializing express and feathers
-const app = express(feathers());
-
-// adding the ability to parse json
-app.use(express.json());
-
-//config socket io in realtion APIs
-app.configure(socketio());
-
-//Enable rest services
-app.configure(express.rest());
-
-// register services
-
-app.use('/search', new SearchService());
-
-// new connections connect to stream
-app.on('connection', conn => app.channel('stream').join(conn));
-
-// publish to stream
-app.publish(data => app.channel('stream'));
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
 
 const results = news.search("bitcoin");
+// define a simple route
+app.get('/', (req, res) => {
+    res.json(results);
+});
+
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
 
 app.listen(PORT).on('listening', () => {
     console.log(`Realtime server running on ${PORT} `);
@@ -54,31 +35,5 @@ app.listen(PORT).on('listening', () => {
 
 
 
-app.io.on("connection", socket => {
-  
-  console.log('connected socket',socket.id);
-    
-  // disconnect
-  socket.on("disconnect", data => {
-    console.log("Disconnected : ",socket.id);
-  });
-
-  // send message
-  socket.on("search", data => {            
-    
-    news.search(data).then(results => {
-      socket.emit("search", results);
-    });
-
-  });
-
-  socket.on("category", data => {    
-    news.refine(data).then(results => {
-      socket.emit("category",results);
-    });
-
-  });
-
-});
 
 // TODO - consider turning this app into a simple crud app
