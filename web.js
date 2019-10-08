47011b747a689416a332ae1fe1e8b0a3a849b325
justice <mobiusndou@gmail.com>
2019-10-08 21:35:43 +0200

    commit

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3030;
 const redis = require('redis');
 const cache_config = {redis:process.env.REDIS_URL || config.get('redis') };
 const cache = require("express-redis-cache")({
-  client: redis.createClient(cache_config)
+  client: redis.createClient(cache_config.redis)
 });
 
 cache.on("connected", () => {
